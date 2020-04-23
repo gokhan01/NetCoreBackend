da3569e97307164728661028ab0aaa214984aed0
Gökhan Uyar <gokhan.uyar@polynomtech.com>
2020-04-23 13:02:41 +0300

    BLL - Autofac impl.

---
+++ NetCoreBackend.BLL/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using NetCoreBackend.BLL.Abstract;
+using NetCoreBackend.BLL.Concrete;
+using NetCoreBackend.DAL.Abstract;
+using NetCoreBackend.DAL.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetCoreBackend.BLL.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ProductManager>().As<IProductService>();
+            builder.RegisterType<EfProductDal>().As<IProductDal>();
+        }
+    }
+}
