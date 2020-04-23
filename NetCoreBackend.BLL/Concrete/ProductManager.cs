5c0a6c6dadf83e1fca71fed1e68dbae550931b6f
Gökhan Uyar <gokhan.uyar@polynomtech.com>
2020-04-23 13:02:16 +0300

    Constant messages created

--- NetCoreBackend.BLL/Concrete/ProductManager.cs
+++ NetCoreBackend.BLL/Concrete/ProductManager.cs
@@ -1,4 +1,5 @@
 ﻿using NetCoreBackend.BLL.Abstract;
+using NetCoreBackend.BLL.Constants;
 using NetCoreBackend.Core.Utilities.Results;
 using NetCoreBackend.DAL.Abstract;
 using NetCoreBackend.Entities.Concrete;
@@ -12,17 +13,6 @@ namespace NetCoreBackend.BLL.Concrete
         public ProductManager(IProductDal productDal)
         {
             _productDal = productDal;
-        }
-
-        public IDataResult<Product> Add(Product product)
-        {
-            return new SuccessDataResult<Product>(_productDal.Add(product));
-        }
-
-        public IResult Delete(Product product)
-        {
-            _productDal.Delete(product);
-            return new SuccessResult("Başarılı");
         }
 
         public IDataResult<Product> GetById(int productId)
@@ -40,10 +30,21 @@ namespace NetCoreBackend.BLL.Concrete
             return new SuccessDataResult<List<Product>>(_productDal.GetList(x => x.CategoryID == categoryId));
         }
 
+        public IDataResult<Product> Add(Product product)
+        {
+            return new SuccessDataResult<Product>(_productDal.Add(product));
+        }
+
+        public IResult Delete(Product product)
+        {
+            _productDal.Delete(product);
+            return new SuccessResult(Messages.ProductDeleted);
+        }        
+
         public IResult Update(Product product)
         {
             _productDal.Update(product);
-            return new SuccessResult("Başarılı");
+            return new SuccessResult(Messages.ProductUpdated);
         }
     }
 }
---
+++ NetCoreBackend.BLL/Constants/Messages.cs
@@ -0,0 +1,8 @@
+﻿namespace NetCoreBackend.BLL.Constants
+{
+    public static class Messages
+    {
+        public static string ProductUpdated = "Güncellendi";
+        public static string ProductDeleted = "Silindi";
+    }
+}
