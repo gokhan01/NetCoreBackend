364a4a382bcca8cf4d3151b31d4d831cdf9e50b9
Gökhan Uyar <gokhan.uyar@polynomtech.com>
2020-04-23 15:57:06 +0300

    user dto's created

---
+++ NetCoreBackend.Entities/Dtos/UserForLoginDto.cs
@@ -0,0 +1,10 @@
+﻿using NetCoreBackend.Core.Entities;
+
+namespace NetCoreBackend.Entities.Dtos
+{
+    public class UserForLoginDto : IDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ NetCoreBackend.Entities/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,12 @@
+﻿using NetCoreBackend.Core.Entities;
+
+namespace NetCoreBackend.Entities.Dtos
+{
+    public class UserForRegisterDto : IDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
