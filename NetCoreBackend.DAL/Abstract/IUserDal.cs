4ec5a781742c16eba8e0b89c3f3725aaff020dbf
Gökhan Uyar <gokhan.uyar@polynomtech.com>
2020-04-23 15:00:43 +0300

    Role manage - dal conf.

---
+++ NetCoreBackend.DAL/Abstract/IUserDal.cs
@@ -0,0 +1,13 @@
+﻿using NetCoreBackend.Core.DAL;
+using NetCoreBackend.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetCoreBackend.DAL.Abstract
+{
+    public interface IUserDal : IEntityRepository<User>
+    {
+        List<OperationClaim> GetClaims(User user);
+    }
+}
--- NetCoreBackend.DAL/Concrete/EntityFramework/Contexts/NorthwindContext.cs
+++ NetCoreBackend.DAL/Concrete/EntityFramework/Contexts/NorthwindContext.cs
@@ -12,5 +12,8 @@ namespace NetCoreBackend.DAL.Concrete.EntityFramework.Contexts
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<OperationClaim> OperationClaims { get; set; }
+        public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
     }
 }
