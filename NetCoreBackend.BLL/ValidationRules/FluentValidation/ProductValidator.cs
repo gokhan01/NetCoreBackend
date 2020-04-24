a8ee8044816c3cc2fd758ca00a7db64a4c253cfc
Gökhan Uyar <gokhan.uyar@polynomtech.com>
2020-04-24 18:09:47 +0300

    Bll - ProductValidator created

---
+++ NetCoreBackend.BLL/ValidationRules/FluentValidation/ProductValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using NetCoreBackend.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NetCoreBackend.BLL.ValidationRules.FluentValidation
+{
+    public class ProductValidator : AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(x => x.ProductName).NotEmpty();
+            RuleFor(x => x.ProductName).Length(2, 30);
+            RuleFor(x => x.UnitPrice).NotEmpty();
+            RuleFor(x => x.UnitPrice).GreaterThanOrEqualTo(1);
+            RuleFor(x => x.UnitPrice).GreaterThanOrEqualTo(10).When(x => x.CategoryID == 1);
+            RuleFor(x => x.ProductName).Must(StartWithA);//custom
+        }
+
+        private bool StartWithA(string arg)
+        {
+            return arg.StartsWith("A");
+        }
+    }
+}
