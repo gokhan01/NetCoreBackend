a752287b80923375ac9b9841539fe0da92be5f1b
Gökhan Uyar <gokhan.uyar@polynomtech.com>
2020-04-25 19:29:18 +0300

    core - AspectInterceptorSelector created for Autofac

---
+++ NetCoreBackend.Core/Utilities/Interceptors/AspectInterceptorSelector.cs
@@ -0,0 +1,22 @@
+﻿using Castle.DynamicProxy;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace NetCoreBackend.Core.Utilities.Interceptors
+{
+    //Autofac
+    public class AspectInterceptorSelector : IInterceptorSelector
+    {
+        public IInterceptor[] SelectInterceptors(Type type, MethodInfo method, IInterceptor[] interceptors)
+        {
+            var classAttributes = type.GetCustomAttributes<MethodInterceptionBaseAttribute>(true).ToList();
+
+            var methodAtributes = type.GetMethod(method.Name).GetCustomAttributes<MethodInterceptionBaseAttribute>(true);
+
+            classAttributes.AddRange(methodAtributes);
+
+            return classAttributes.OrderBy(x => x.Priority).ToArray();
+        }
+    }
+}
--- NetCoreBackend.Core/Utilities/Interceptors/MethodInterceptionBaseAttribute.cs
+++ NetCoreBackend.Core/Utilities/Interceptors/MethodInterceptionBaseAttribute.cs
@@ -1,18 +1,21 @@
-﻿using Castle.Core.Interceptor;
+﻿using Castle.DynamicProxy;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace NetCoreBackend.Core.Utilities.Interceptors
 {
+    //Attribute için kullanılabilecek alanlar ve nasıl kullanılabilecekleri set edilebilir
+    //Class seviyesinde
+    //Metodlarda,
+    //Birden fazla
+    //Sub class'larda kullanılabilir
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
     public abstract class MethodInterceptionBaseAttribute : Attribute, IInterceptor
     {
+        //Görev sırası bu özellik ile yönetilecek
         public int Priority { get; set; }
 
         public virtual void Intercept(IInvocation invocation)
         {
-            throw new NotImplementedException();
         }
     }
 }
