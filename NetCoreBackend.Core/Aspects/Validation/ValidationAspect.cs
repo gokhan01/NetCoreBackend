f0fc5dd66f747545ac7459c895aaaa66ab033563
Gökhan Uyar <gokhan.uyar@polynomtech.com>
2020-04-25 19:27:36 +0300

    core - AspectMessages created

---
+++ NetCoreBackend.Core/Aspects/Validation/ValidationAspect.cs
@@ -0,0 +1,42 @@
+﻿using Castle.DynamicProxy;
+using FluentValidation;
+using NetCoreBackend.Core.CrossCuttingConcerns.Validation;
+using NetCoreBackend.Core.Utilities.Interceptors;
+using NetCoreBackend.Core.Utilities.Messages;
+using System;
+using System.Linq;
+
+namespace NetCoreBackend.Core.Aspects.Validation
+{
+    //Bu sınıf Autofac ve FluentValidation'a bağımlı
+    public class ValidationAspect : MethodInterception
+    {
+        Type _validatorType;
+
+        //Örnek => ProductValidator : AbstractValidator<Product>
+        //FluentValidation'a ait AbstractValidator sınıfı IValidator interface'inden türemiştir
+        public ValidationAspect(Type validatorType)
+        {
+            //Gelen tipin FluentValidation.IValidator interface'ine atanabilirlik kontrolü yapılır
+            if (!typeof(IValidator).IsAssignableFrom(validatorType))
+            {
+                throw new Exception(AspectMessages.WrongValidationType);
+            }
+            _validatorType = validatorType;
+        }
+        protected override void OnBefore(IInvocation invocation)
+        {
+            //Validator sınıfı için yeni örnek oluşturulur
+            var validator = (IValidator)Activator.CreateInstance(_validatorType);
+            //Validator sınıfının türediği AbstractValidator'a generic olarak gönderilen tip ele alınır 
+            var entityType = _validatorType.BaseType.GetGenericArguments()[0];
+            //Metod argümanları alınır. Örnek => Add(Product product)
+            var entities = invocation.Arguments.Where(t => t.GetType() == entityType);
+
+            foreach (var item in entities)
+            {
+                ValidationTool.Validate(validator, entityType);
+            }
+        }
+    }
+}
---
+++ NetCoreBackend.Core/Utilities/Messages/AspectMessages.cs
@@ -0,0 +1,7 @@
+﻿namespace NetCoreBackend.Core.Utilities.Messages
+{
+    public static class AspectMessages
+    {
+        public static string WrongValidationType = "Wrong validation type";
+    }
+}
