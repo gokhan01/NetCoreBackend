4d834078967fdb85f7e4db1131451eb1fe455904
Gökhan Uyar <gokhan.uyar@polynomtech.com>
2020-04-26 19:21:09 +0300

    JwtHelper bug fix

--- NetCoreBackend.Core/Utilities/Security/Jwt/JwtHelper.cs
+++ NetCoreBackend.Core/Utilities/Security/Jwt/JwtHelper.cs
@@ -20,11 +20,11 @@ namespace NetCoreBackend.Core.Utilities.Security.Jwt
         {
             Configuration = configuration;
             _tokenOptions = Configuration.GetSection("TokenOptions").Get<TokenOptions>();
-            _accessTokenExpiration = DateTime.Now.AddMinutes(_tokenOptions.AccessTokenExpiration);
         }
 
         public AccessToken CreateToken(User user, List<OperationClaim> operationClaims)
         {
+            _accessTokenExpiration = DateTime.Now.AddMinutes(_tokenOptions.AccessTokenExpiration);
             var securityKey = SecurityKeyHelper.CreateSecurityKey(_tokenOptions.SecurityKey);
             var signingCredentials = SigningCredentialsHelper.CreateSigningCredentials(securityKey);
             JwtSecurityToken jwt = CreateJwtSecurityToken(_tokenOptions, user, signingCredentials, operationClaims);
