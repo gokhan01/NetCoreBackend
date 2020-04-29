afc5923ea030bef855864b242f33b57cae7cafef
Gökhan <gokhanuyar@hotmail.com>
2020-04-29 18:33:26 +0300

    ExceptionLogAspect bug fix

--- NetCoreBackend.Core/Aspects/Autofac/Exception/ExceptionLogAspect.cs
+++ NetCoreBackend.Core/Aspects/Autofac/Exception/ExceptionLogAspect.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Castle.DynamicProxy;
 using NetCoreBackend.Core.CrossCuttingConcerns.Logging;
 using NetCoreBackend.Core.CrossCuttingConcerns.Logging.Log4Net;
+using NetCoreBackend.Core.CrossCuttingConcerns.Logging.SeriLog;
 using NetCoreBackend.Core.Utilities.Interceptors;
 using NetCoreBackend.Core.Utilities.Messages;
 
@@ -10,16 +11,16 @@ namespace NetCoreBackend.Core.Aspects.Autofac.Exception
 {
     public class ExceptionLogAspect : MethodInterception
     {
-        private LoggerServiceBase _loggerServiceBase;
+        private SerilogServiceBase _loggerServiceBase;
 
         public ExceptionLogAspect(Type loggerService)
         {
-            if (loggerService.BaseType != typeof(LoggerServiceBase))
+            if (loggerService.BaseType != typeof(SerilogServiceBase))
             {
                 throw new System.Exception(AspectMessages.WrongLoggerType);
             }
 
-            _loggerServiceBase = (LoggerServiceBase)Activator.CreateInstance(loggerService);
+            _loggerServiceBase = (SerilogServiceBase)Activator.CreateInstance(loggerService);
         }
         protected override void OnException(IInvocation invocation, System.Exception e)
         {
